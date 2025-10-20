f339db9e2e6c02d91584b6c2a2d86d32881650e3
rocktim <rocktimsharma82@gmail.com>
2025-10-20 11:10:07 +0530

    add page sub header component

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -6,7 +6,9 @@ import RegisterPage from "@features/auth/pages/RegisterPage.tsx";
 import AuthLayout from "@components/layout/AuthLayout.tsx";
 import DashboardLayout from "@components/layout/DashboardLayout.tsx";
 import HomePage from "@features/home/pages/HomePage.tsx";
-import ProductPage from "@features/Product/pages";
+
+import ProductCreatePage from "@features/Product/pages/create.tsx";
+import ProductsPage from "@features/Product/pages";
 
 export const router = createBrowserRouter([
     {
@@ -20,7 +22,15 @@ export const router = createBrowserRouter([
         element: <DashboardLayout/>,   // with navbar/footer
         children: [
             {path: "/", element: <HomePage/>},
-            {path: "products", element: <ProductPage/>},
+            {
+                path: "products",
+                children: [
+                    {index: true, element: <ProductsPage/>},               // /products
+                    {path: "new", element: <ProductCreatePage/>},          // /products/new
+                    // { path: ":id", element: <ProductDetailsPage /> },         // /products/:id
+                    // { path: ":id/edit", element: <ProductEditPage /> },       // /products/:id/edit
+                ],
+            },
             // dashboard/product routes go here
         ],
     },
